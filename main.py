891431c6c9fbaca8424078f0e50a8faa922b8ec6
Meghani M. Mehboob <suitable9004@gmail.com>
2021-06-25 18:11:52 +0530

    Fixed Naming Conventions

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ Created on Thu Jun 24 23:39:38 2021
 @author: Admin
 """
 import random
+from termcolor import colored
 
-playerscore = 0
+PLAYER_SCORE = 0
 
 snakes = [
     {15: 12},
@@ -21,35 +22,44 @@ ladders = [
 ]
 
 
-def mainexit():
-    global playerscore
-    print(playerscore)
+def snake_ladder(player_score):
+    """
+    Parameters
+    ----------
+    player_score : TYPE
+        DESCRIPTION.
 
+    Returns
+    -------
+    None.
 
-def checksnakeandladder():
-    global playerscore
-    checkladder = True
+    """
+    check_ladder = True
 
     for snake in snakes:
-        if playerscore in snake.keys():
-            playerscore = snake[playerscore]
-            checkladder = False
+        if player_score in snake.keys():
+            player_score = snake[player_score]
+            check_ladder = False
+            print(colored("Snake Found Demoted to " + str(player_score), "red"))
             break
 
-    if checkladder:
+    if check_ladder:
         for ladder in ladders:
-            if playerscore in ladder.keys():
-                playerscore = ladder[playerscore]
+            if player_score in ladder.keys():
+                player_score = ladder[player_score]
+                print(colored("Ladder Found Promoted to " + str(player_score), "green"))
                 break
 
-    mainexit()
+    return player_score
 
 
 if __name__ == "__main__":
-    while playerscore < 100:
-        dicevalue = random.randint(1, 6)
-        if playerscore + dicevalue <= 100:
-            playerscore = playerscore + dicevalue
-            checksnakeandladder()
+    while PLAYER_SCORE < 100:
+        DICE_VALUE = random.randint(1, 6)
+        print("The Diced", DICE_VALUE)
+        if PLAYER_SCORE + DICE_VALUE <= 100:
+            PLAYER_SCORE = PLAYER_SCORE + DICE_VALUE
+            PLAYER_SCORE = snake_ladder(PLAYER_SCORE)
+            print("The Player Score", PLAYER_SCORE)
         else:
             pass
